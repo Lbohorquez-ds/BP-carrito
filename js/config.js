0911002ae06ba65ac0ea7a606739c24b7586016b
Lbohorquez-ds <36065324@ifts24.edu.ar>
2026-09-07 14:24:22 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// NDB + Firebase
+// Proyecto: CarritoNDB
+window.NDB_CONFIG = {
+  // UID del administrador principal de NDB
+  adminUid: "3Vx8WDmJiVTEWfOfsSjycelHDRs1",
+  firebaseConfig: {
+    apiKey: "AIzaSyC7vFDvSeUSQcbatpxsTRBWX_kLdanQ2vA",
+    authDomain: "carritondb.firebaseapp.com",
+    projectId: "carritondb",
+    storageBucket: "carritondb.firebasestorage.app",
+    messagingSenderId: "1080300012200",
+    appId: "1:1080300012200:web:22e588cee790259081389d"
+  }
+};
